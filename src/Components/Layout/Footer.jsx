92ef54d2515dc4bdb16ef3eb012b27c602e62691
Animes <animesbarman101@gmail.com>
2025-10-29 18:45:52 +0600

    add login

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -2,58 +2,28 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="p-6 dark:bg-gray-100 dark:text-gray-800">
-        <div className="container grid grid-cols-2 mx-auto gap-x-3 gap-y-8 sm:grid-cols-3 md:grid-cols-4">
-          <div className="flex flex-col space-y-4">
-            <h2 className="font-medium">Getting started</h2>
-            <div className="flex flex-col space-y-2 text-sm dark:text-gray-600">
-              <a rel="noopener noreferrer" href="#">Installation</a>
-              <a rel="noopener noreferrer" href="#">Release Notes</a>
-              <a rel="noopener noreferrer" href="#">Upgrade Guide</a>
-              <a rel="noopener noreferrer" href="#">Using with Preprocessors</a>
-              <a rel="noopener noreferrer" href="#">Optimizing for Production</a>
-              <a rel="noopener noreferrer" href="#">Browser Support</a>
-              <a rel="noopener noreferrer" href="#">IntelliSense</a>
-            </div>
-          </div>
-          <div className="flex flex-col space-y-4">
-            <h2 className="font-medium">Core Concepts</h2>
-            <div className="flex flex-col space-y-2 text-sm dark:text-gray-600">
-              <a rel="noopener noreferrer" href="#">Utility-First</a>
-              <a rel="noopener noreferrer" href="#">Responsive Design</a>
-              <a rel="noopener noreferrer" href="#">Hover, Focus, &amp; Other States</a>
-              <a rel="noopener noreferrer" href="#">Dark Mode</a>
-              <a rel="noopener noreferrer" href="#">Adding Base Styles</a>
-              <a rel="noopener noreferrer" href="#">Extracting Components</a>
-              <a rel="noopener noreferrer" href="#">Adding New Utilities</a>
-            </div>
-          </div>
-          <div className="flex flex-col space-y-4">
-            <h2 className="font-medium">Customization</h2>
-            <div className="flex flex-col space-y-2 text-sm dark:text-gray-600">
-              <a rel="noopener noreferrer" href="#">Configuration</a>
-              <a rel="noopener noreferrer" href="#">Theme Configuration</a>
-              <a rel="noopener noreferrer" href="#">Breakpoints</a>
-              <a rel="noopener noreferrer" href="#">Customizing Colors</a>
-              <a rel="noopener noreferrer" href="#">Customizing Spacing</a>
-              <a rel="noopener noreferrer" href="#">Configuring Variants</a>
-              <a rel="noopener noreferrer" href="#">Plugins</a>
-            </div>
-          </div>
-          <div className="flex flex-col space-y-4">
-            <h2 className="font-medium">Community</h2>
-            <div className="flex flex-col space-y-2 text-sm dark:text-gray-600">
-              <a rel="noopener noreferrer" href="#">GitHub</a>
-              <a rel="noopener noreferrer" href="#">Discord</a>
-              <a rel="noopener noreferrer" href="#">Twitter</a>
-              <a rel="noopener noreferrer" href="#">YouTube</a>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center justify-center px-6 pt-12 text-sm">
-          <span className="dark:text-gray-600">© Copyright 1986. All Rights Reserved.</span>
-        </div>
-      </footer>
+   <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
+  <nav>
+    <h6 className="footer-title">Services</h6>
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Company</h6>
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Press kit</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Legal</h6>
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
 
   )
 }
