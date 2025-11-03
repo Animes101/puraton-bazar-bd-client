d20ac8f6028dc0554fc87fabae66464403d43ae7
Animes <animesbarman101@gmail.com>
2025-11-03 15:29:35 +0600

    add some

---
+++ src/hooks/useItem.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+
+const useItem = () => {
+    const [product, setProduct]=useState(null);
+
+
+    useEffect(()=>{
+
+
+    })
+  return {product}
+}
+
+export default useItem
