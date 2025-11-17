d949ed5ea0a9b53debc886db4148035c1038957d
Animes <animesbarman101@gmail.com>
2025-11-17 13:45:45 +0600

    payment history

--- src/Pages/Payment.jsx
+++ src/Pages/Payment.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthProvider';
+import useAxiosSecure from '../hooks/useAxiosSecure';
 
 const Payment = () => {
+
+  const axiosSecure=useAxiosSecure();
+
+  const {user}=useContext(AuthContext);
+
+  const {data, isLoading}=useQuery({
+        queryKey:['payment', user?.email],
+        queryFn:async()=>{
+            const res=await axiosSecure.get(`/paymentHistory/${user?.email}`);
+            return res.data.data;
+        }
+    })
+
+    console.log(data);
+
   return (
     <div>Payment</div>
   )
