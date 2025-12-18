2d95185d42a35950ec95e5a14989c634908fd769
Animes <animesbarman101@gmail.com>
2025-12-18 10:53:44 +0600

    version-1

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthProvider";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://puratonbazarserver.vercel.app",
 });
 
 const useAxiosSecure = () => {
